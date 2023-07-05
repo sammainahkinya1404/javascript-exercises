a9eeaa206bea2f3557fac9fef59cef9f8793df7d
Samson Kinyanjui <sammainah98@gmail.com>
2023-07-05 21:30:15 +0300

    We have unfinished Business

--- 01_helloWorld/helloWorld.js
+++ 01_helloWorld/helloWorld.js
@@ -1,5 +1,5 @@
 const helloWorld = function() {
-  return ''
+  return 'hello World'
 };
 
 module.exports = helloWorld;
--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,10 +1,21 @@
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
+let time3=3;
+let word1="hey";
+ function  repeatString (word, times) {
+  word=word1;
+  times=time3;
+
+
+  if (times < 0){
+    return "ERROR";
+  }
+  else{
+    let string = "";
   for (let i = 0; i < times; i++) {
     string += word;
-  }
+  
   return string;
+  }
+  
 };
-
+}
 module.exports = repeatString;
